a72c26fbe8c700bafb7e5258f73559097689ad22
Gargi Chaudhary <gargichaudhary06@gmail.com>
2022-02-06 08:34:58 +0530

    Create 2095-delete-the-middle-node-of-a-linked-list.cpp

---
+++ 2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -0,0 +1,38 @@
+"""
+Problem link: https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+Example:
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+"""
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            head = NULL;
+            return head;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = slow;
+        while(fast != NULL && fast -> next != NULL){
+            prev = slow;
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        prev -> next = slow -> next;
+        return head;
+    }
+};
