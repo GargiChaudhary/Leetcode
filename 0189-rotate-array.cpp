a231bad56de2f467a0ba08a6654970fa3d6dbde1
Gargi Chaudhary <gargichaudhary06@gmail.com>
2022-03-03 20:33:20 +0530

    Update 0189-rotate-array.cpp

--- 0189-rotate-array.cpp
+++ 0189-rotate-array.cpp
@@ -22,3 +22,14 @@ public:
         nums = num;
     }
 };
+
+//Second Approach
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> ans(nums.size());
+        for(int i = 0; i < nums.size(); i++)
+            ans[(i+k)%nums.size()] = nums[i];
+        nums = ans;
+    }
+};
